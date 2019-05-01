54cef99efc862b513ea37d267ecf9d77bb5b8d61
Strating <Michael.Strating@dematic.com>
2019-05-01 10:46:27 -0400

    removed all bot logic, to restart

--- bot.py
+++ bot.py
@@ -1,9 +1,5 @@
 import discord
-import os
 
-from ability_search import search_for_ability
-from exceptions import ItemSearchError, AbilitySearchError
-from item_search import search_for_item
 from user_vals import TOKEN
 
 TOKEN = TOKEN
@@ -19,25 +15,7 @@ async def on_message(message):
     if message.content.startswith('!item'):
         search = message.content.replace('!item ', '')
         channel = message.channel
-        try:
-            img = search_for_item(search)
-        except ItemSearchError as e:
-            await channel.send('{}'.format(e))
-        else:
-            await channel.send(file=discord.File(img))
-            os.remove(img)
-
-
-    if message.content.startswith('!ability'):
-        search = message.content.replace('!ability ', '')
-        channel = message.channel
-        try:
-            msgs = search_for_ability(search)
-        except AbilitySearchError as e:
-            await channel.send(e)
-        else:
-            for msg in msgs:
-                await channel.send(embed=msg)
+        await channel.send('The bot is currently under development and is not functional')
 
 
 @client.event
