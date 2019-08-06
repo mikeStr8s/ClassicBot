be7535b29cb7a6670b278370dadbc45f24c003a9
unknown <michaelstrating@hotmail.com>
2019-08-06 19:49:00 -0400

    Altered string formatting to allow for locale

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 ITEM_URL = 'https://classic.wowhead.com/item={0}'
 
 OPEN_SEARCH = 'http://{}classic.wowhead.com/search?q={}&opensearch'
-TOOLTIP = 'https://classic.wowhead.com/tooltip/{}/{}&json&power'
+TOOLTIP = 'https://{}classic.wowhead.com/tooltip/{}/{}&json&power'
 ICON = 'https://wow.zamimg.com/images/wow/icons/large/{0}.jpg'
 MONEY = 'https://wow.zamimg.com/images/icons/money-{}.gif'
 
